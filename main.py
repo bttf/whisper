6955d793ba621633295f7fe000cb76d6f45e362f
bttf <2374625+bttf@users.noreply.github.com>
2024-01-14 10:25:38 -0800

    switch to onedir build for faster boottime; fix multiprocessing bug; switch to requiring out_file path and output results in json file

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import argparse
 import whisper
 import json
+import sys
+import multiprocessing
 
-def main(video_path):
+def main(video_path, out_file):
     model = whisper.load_model('base')
     result = model.transcribe(video_path, verbose=True)
-    print(json.dumps(result, indent=2))
+    with open(out_file, 'w') as f:
+        json.dump(result, f, indent=2)
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     parser = argparse.ArgumentParser(description='Transcribe a video file.')
     parser.add_argument('video_path', type=str, help='The path to the video or audio file.')
+    parser.add_argument('out_file', type=str, help='The path to the output json file.')
     
     args = parser.parse_args()
-    main(args.video_path)
+    main(args.video_path, args.out_file)
